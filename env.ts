86b735b760f21db0c15a1021271c44d32d5ac65d
MannuVilasara <mannuvilasara@gmail.com>
2025-06-06 20:53:15 +0530

    rm -rf *

--- env.ts
+++
@@ -1,4 +0,0 @@
-export const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID as string;
-export const SPOTIFY_CLIENT_SECRET = process.env
-  .SPOTIFY_CLIENT_SECRET as string;
-export const REFRESH_TOKEN = process.env.REFRESH_TOKEN as string;
