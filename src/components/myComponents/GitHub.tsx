c769d0edbe41f6bdd47409ad5c0347a14152f058
MannuVilasara <mannuvilasara@gmail.com>
2025-06-09 00:06:37 +0530

    fix:🐈 colorscheme (gh)

--- src/components/myComponents/GitHub.tsx
+++ src/components/myComponents/GitHub.tsx
@@ -5,6 +5,17 @@ import GitHubCalendar from 'react-github-calendar';
 import { Row } from 'react-bootstrap';
 import { useTheme } from 'next-themes';
 
+// Define ThemeInput type for explicitTheme
+type ThemeInput = {
+  light: string[];
+  dark: string[];
+};
+
+// NOTE: USE THIS THEME LATER
+const minimalTheme: ThemeInput = {
+  light: ['hsl(0, 0%, 92%)', 'rebeccapurple'],
+  dark: ['hsl(0, 0%, 20%)', 'rebeccapurple'],
+};
 function Github() {
   const { theme } = useTheme();
   const [isDarkMode, setIsDarkMode] = useState(false);
