f20ec6cb01feb6978993df205627075e38773de2
MannuVilasara <mannuvilasara@gmail.com>
2025-06-10 21:25:29 +0530

    feat:⚒ Projects

--- src/components/myComponents/ProjectLinks.tsx
+++ src/components/myComponents/ProjectLinks.tsx
@@ -1,121 +1,53 @@
-'use client';
+import Image from 'next/image';
+import { ExternalLink } from 'lucide-react';
 
-import { FiArrowRight } from 'react-icons/fi';
-import { useMotionValue, motion, useSpring, useTransform } from 'framer-motion';
-import React, { useRef } from 'react';
-
-type ProjectLinkProps = {
-  heading: string;
-  imgSrc: string;
-  subheading: string;
-  href: string;
-  target?: string;
-};
-
-export const ProjectLink = ({ heading, imgSrc, subheading, href, target }: ProjectLinkProps) => {
-  const ref = useRef<HTMLAnchorElement>(null);
-
-  const x = useMotionValue(0);
-  const y = useMotionValue(0);
-
-  const mouseXSpring = useSpring(x, { stiffness: 300, damping: 30 });
-  const mouseYSpring = useSpring(y, { stiffness: 300, damping: 30 });
-
-  const top = useTransform(mouseYSpring, [0.5, -0.5], ['40%', '60%']);
-  const left = useTransform(mouseXSpring, [0.5, -0.5], ['60%', '70%']);
-
-  const handleMouseMove = (e: React.MouseEvent<HTMLAnchorElement>) => {
-    if (!ref.current) return;
-    const rect = ref.current.getBoundingClientRect();
-
-    const width = rect.width;
-    const height = rect.height;
-
-    const mouseX = e.clientX - rect.left;
-    const mouseY = e.clientY - rect.top;
-
-    const xPct = mouseX / width - 0.5;
-    const yPct = mouseY / height - 0.5;
-
-    x.set(xPct);
-    y.set(yPct);
-  };
-
+export function ProjectLink({
+  heading,
+  subheading,
+  color,
+  language,
+  stars,
+  href,
+  target = '_blank',
+}: any) {
   return (
-    <motion.a
+    <a
       href={href}
-      ref={ref}
       target={target}
-      onMouseMove={handleMouseMove}
-      initial="initial"
-      whileHover="whileHover"
-      className="group relative flex items-center justify-between border-b-2 py-6 transition-colors duration-500 hover:text-foreground cursor-pointer md:py-8"
+      rel="noopener noreferrer"
+      className="rounded-lg border mono border-neutral-200 dark:border-neutral-800 bg-neutral-50 dark:bg-neutral-900 p-5 transition-colors hover:bg-neutral-200 dark:hover:bg-neutral-800 flex flex-col justify-between"
     >
-      <div>
-        <motion.span
-          variants={{
-            initial: { x: 0 },
-            whileHover: { x: -20 },
-          }}
-          transition={{
-            type: 'spring',
-            staggerChildren: 0.05,
-            delayChildren: 0.25,
-          }}
-          className="relative z-10 block text-4xl font-bold transition-colors duration-500  md:text-4xl"
-        >
-          {heading.split('').map((l, i) => (
-            <motion.span
-              variants={{
-                initial: { x: 0 },
-                whileHover: { x: 20 },
-              }}
-              transition={{ type: 'spring' }}
-              className="inline-block"
-              key={i}
-            >
-              {l}
-            </motion.span>
-          ))}
-        </motion.span>
-        <span className="relative z-10 mt-2 block text-base transition-colors duration-500  max-w-lg mono">
-          {subheading}
-        </span>
+      <div className="flex items-center justify-between mb-2">
+        <div className="flex items-center gap-2">
+          <Image
+            src="/android-chrome-512x512.png"
+            alt="avatar"
+            width={20}
+            height={20}
+            className="rounded-full"
+          />
+          <span className="text-sm text-neutral-800 dark:text-neutral-400 font-mono">
+            MannuVilasara
+          </span>
+        </div>
+        <ExternalLink size={16} className="text-neutral-800 dark:text-neutral-500" />
       </div>
 
-      <motion.img
-        style={{
-          top,
-          left,
-          translateX: '-50%',
-          translateY: '-50%',
-        }}
-        variants={{
-          initial: { scale: 0, rotate: '-10deg' },
-          whileHover: { scale: 1, rotate: '10deg' },
-        }}
-        transition={{ type: 'spring' }}
-        src={imgSrc}
-        alt={`Image for project ${heading}`}
-        className="absolute z-20 h-28 w-48 rounded-lg object-cover md:h-48 md:w-64"
-      />
+      <h2 className="text-lg font-semibold text-neutral-900 dark:text-neutral-100 mb-1">
+        {heading}
+      </h2>
+      <p className="text-sm text-neutral-500 dark:text-neutral-300 mb-4">{subheading}</p>
 
-      <motion.div
-        variants={{
-          initial: {
-            x: '30%',
-            opacity: 0,
-          },
-          whileHover: {
-            x: '0%',
-            opacity: 1,
-          },
-        }}
-        transition={{ type: 'spring' }}
-        className="relative z-10 p-4"
-      >
-        <FiArrowRight className="text-4xl " />
-      </motion.div>
-    </motion.a>
+      <div className="flex items-center gap-4 text-sm text-neutral-400">
+        <div className="flex items-center gap-1">
+          <span className={`h-2 w-2 rounded-full ${color}`} />
+          <span>{language}</span>
+        </div>
+        <div className="flex items-center gap-1">
+          <span>⭐</span>
+          <span>{stars}</span>
+        </div>
+      </div>
+    </a>
   );
-};
+}
